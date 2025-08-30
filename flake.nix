c2c2af3a38adedecae69c0edc9499a8951e8f2ab
Kent Smith <kentsmith@gmail.com>
2025-08-29 19:46:08 -0700

    Add flake for development environment
    Uses unstable to use zig 0.15.1.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "ulzig";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable-small/nixexprs.tar.xz";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  in
+    builtins.foldl' nixpkgs.lib.recursiveUpdate {} (
+      builtins.map (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShells.${system}.default = pkgs.mkShell {
+            packages = with pkgs;
+              [
+                elfkickers
+                zig_0_15
+              ]
+              ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [kcov]);
+          };
+
+          formatter.${system} = pkgs.alejandra;
+        }
+      )
+      systems
+    );
+}
