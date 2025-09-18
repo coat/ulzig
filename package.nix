5918d8ab3948b146a3497a260a654309f04e80fa
Kent Smith <kentsmith@gmail.com>
2025-09-17 17:25:11 -0700

    bump version

--- package.nix
+++ package.nix
@@ -12,7 +12,7 @@ in
   stdenvNoCC.mkDerivation (
     finalAttrs: {
       name = "ulz";
-      version = "0.1.0";
+      version = "0.2.0";
       src = lib.cleanSource ./.;
       nativeBuildInputs = [
         zig_hook
