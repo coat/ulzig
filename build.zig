db88664f9edb68dc0ae6ea94e3bb3f63fcfc889a
Kent Smith <kentsmith@gmail.com>
2025-09-27 16:11:50 -0700

    Adds code coverage
    Adds coverage build option to run kcov on module's test runner. Sets the
    module to use llvm if coverage is set, since kcov doesn't seem to work
    with the new x86_64 zig backend.
    
    Adds github workflow to upload to results to codecov.io.

--- build.zig
+++ build.zig
@@ -4,6 +4,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const coverage = b.option(bool, "coverage", "Generate a coverage report with kcov") orelse false;
+
     const mod = b.addModule("ulzig", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -11,12 +13,12 @@ pub fn build(b: *std.Build) void {
 
     const flags_mod = b.dependency("flags", .{
         .target = target,
-        .optimize = optimize,
     }).module("flags");
 
     const exe_mod = b.addModule("exe", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
+        .optimize = optimize,
         .imports = &.{
             .{ .name = "ulz", .module = mod },
             .{ .name = "flags", .module = flags_mod },
@@ -41,9 +43,7 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
+    const mod_tests = b.addTest(.{ .root_module = mod, .use_llvm = true });
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
@@ -56,4 +56,23 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
+
+    if (coverage) {
+        const kcov_bin = b.findProgram(&.{"kcov"}, &.{}) catch "kcov";
+
+        const coverage_step = std.Build.Step.Run.create(b, "run coverage");
+        coverage_step.addArg(kcov_bin);
+        coverage_step.addPrefixedDirectoryArg("--include-pattern=", b.path("src"));
+
+        const coverage_output = coverage_step.addOutputFileArg(".");
+
+        coverage_step.addDirectoryArg(run_mod_tests.producer.?.getEmittedBin());
+
+        const install_coverage = b.addInstallDirectory(.{
+            .source_dir = coverage_output,
+            .install_dir = .{ .custom = "coverage" },
+            .install_subdir = "",
+        });
+        test_step.dependOn(&install_coverage.step);
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -3,10 +3,8 @@
 pub const decode = ulz.decode;
 pub const encode = ulz.encode;
 
-test {
-    _ = ulz;
-}
-
 const ulz = @import("ulz.zig");
 
-const std = @import("std");
+comptime {
+    @import("std").testing.refAllDecls(@This());
+}
